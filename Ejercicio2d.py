f422f6a84b39b1fbafde40734018938527753f16
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-05 16:38:15 -0500

    El programa dibuja los recuadros

--- Ejercicio2d.py
+++ Ejercicio2d.py
@@ -1,2 +1,6 @@
 from interpreter import draw
 from chessPictures import *
+
+# Programa que dibuja recuadros de un tablero de ajedrez
+# A diferencia de Ejercicio2c este cambia los colores
+draw(square.negative().join(square).horizontalRepeat(4))
