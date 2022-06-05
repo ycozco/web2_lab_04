a6151707c8349db3e95a57218002991b1b2c710f
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-05 16:40:37 -0500

    El programa dibuja el semi- tablero de ajedrez

--- Ejercicio2e.py
+++ Ejercicio2e.py
@@ -1,2 +1,8 @@
 from interpreter import draw
 from chessPictures import *
+
+#Image es una linea de cuadros de ajedrez vertical
+image = Picture(square.negative().img+square.img).verticalRepeat(2)
+
+#Esta parte junta la parte negativa de image con image y la repite cuatro veces 
+draw(image.join(image.negative()).horizontalRepeat(4))
