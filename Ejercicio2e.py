98defe75e11e320bb5633d66e75a03024054616e
Vladimir2003-debug <83074130+Vladimir2003-debug@users.noreply.github.com>
2022-06-05 17:42:10 -0500

    Programa que dibuja una linea de un chesstable

--- Ejercicio2e.py
+++ Ejercicio2e.py
@@ -1,8 +1,6 @@
 from interpreter import draw
 from chessPictures import *
 
-#Image es una linea de cuadros de ajedrez vertical
-image = Picture(square.negative().img+square.img).verticalRepeat(2)
-
-#Esta parte junta la parte negativa de image con image y la repite cuatro veces 
-draw(image.join(image.negative()).horizontalRepeat(4))
+# Programa que dibuja recuadros de un tablero de ajedrez
+# A diferencia de Ejercicio2c este cambia los colores
+draw(square.negative().join(square).horizontalRepeat(4))
